d42df83a9a9c411492500a335c3b8067f934b359
chiakai.chang@leediancn.com <chiakai.chang@leediancn.com>
2018-07-26 10:43:57 +0800

    'addMiddleware'

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,42 +3,59 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: "rubyourhands",
+    title: 'rubyourhands',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "rubyouhandssite" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'rubyouhandssite' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: "~/components/loading.vue",
+  loading: '~/components/loading.vue',
   /*
    ** Global CSS
    */
   css: [
     {
-      src: "@assets/style/index.less",
-      lang: "less"
+      src: '@assets/style/index.less',
+      lang: 'less'
     }
   ],
-
-  modules: ["@nuxtjs/axios"],
+  /*
+   ** router
+   */
+  router: {
+    // 每次切換頁面都會 做的事情
+    middleware: ['test'],
+    extendRoutes(routes, resolve) {
+      // 當使用者切換到的目錄不存在時 會出現這一個頁面
+      routes.push({
+        name: 'custom',
+        path: '*',
+        component: resolve(__dirname, 'components/404page.vue')
+      })
+    }
+  },
+  /*
+   ** modules
+   */
+  modules: ['@nuxtjs/axios'],
   axios: {
     proxy: true
     // proxyHeaders: false
   },
   proxy: {
-    "/api/": "https://opendata.cwb.gov.tw/"
+    '/api/': 'https://opendata.cwb.gov.tw/'
   },
   /*
    ** Plugins
    */
   plugins: [
     {
-      src: "@plugins/iview.js"
+      src: '@plugins/iview.js'
     }
   ],
 
@@ -52,12 +69,12 @@ module.exports = {
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: "pre",
+          enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: "eslint-loader",
+          loader: 'eslint-loader',
           exclude: /(node_modules)/
-        });
+        })
       }
     }
   }
-};
+}
