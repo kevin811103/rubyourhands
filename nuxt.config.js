d061675b88d8307f431861a1af99fe6b373748c0
eric.chang1 <eric.chang@promeritage.com.tw>
2018-10-29 17:23:31 +0800

    t

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,61 +3,61 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-     htmlAttrs: {
-     lang="zh-TW"
-        // 不設定會一直跑出翻譯 
+    htmlAttrs: {
+      lang: "zh-Hant"
+      // 不設定會一直跑出翻譯
       // https://www.injerry.com/blog-view.php?PID=5&sn=125
     },
-    title: 'rubyourhands',
+    title: "rubyourhands",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'rubyouhandssite' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "rubyouhandssite" }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: '~/components/loading.vue',
+  loading: "~/components/loading.vue",
   /*
    ** Global CSS
    */
   css: [
     {
-      src: '@assets/style/index.less',
-      lang: 'less'
+      src: "@assets/style/index.less",
+      lang: "less"
     },
-    'swiper/dist/css/swiper.css'
+    "swiper/dist/css/swiper.css"
   ],
   /*
    ** router
    */
   router: {
     // 每次切換頁面都會 做的事情
-    middleware: ['test'],
+    middleware: ["test"],
     extendRoutes(routes, resolve) {
       // 當使用者切換到的目錄不存在時 會出現這一個頁面
       routes.push({
-        name: 'custom',
-        path: '*',
-        component: resolve(__dirname, 'components/404page.vue')
-      })
+        name: "custom",
+        path: "*",
+        component: resolve(__dirname, "components/404page.vue")
+      });
     }
   },
   /*
    ** modules
    */
-  modules: ['@nuxtjs/axios', ['@nuxtjs/moment',['zh-tw']], '@nuxtjs/pwa'],
+  modules: ["@nuxtjs/axios", ["@nuxtjs/moment", ["zh-tw"]], "@nuxtjs/pwa"],
   axios: {
     proxy: true
     // proxyHeaders: false
   },
   proxy: {
-    '/api/': 'https://opendata.cwb.gov.tw/', //  在打api 的時候會變成  https://opendata.cwb.gov.tw/api/
-    '/authinit/': {
-      target: process.env.SESSION_API_URL || 'https://opendata.cwb.gov.tw/',
-      pathRewrite: { '^/authinit/': '' } // 這邊會變成    https://opendata.cwb.gov.tw/   會自己將authinit 取代掉
+    "/api/": "https://opendata.cwb.gov.tw/", //  在打api 的時候會變成  https://opendata.cwb.gov.tw/api/
+    "/authinit/": {
+      target: process.env.SESSION_API_URL || "https://opendata.cwb.gov.tw/",
+      pathRewrite: { "^/authinit/": "" } // 這邊會變成    https://opendata.cwb.gov.tw/   會自己將authinit 取代掉
     }
   },
   /*
@@ -65,21 +65,21 @@ module.exports = {
    */
   plugins: [
     {
-      src: '@plugins/iview.js'
+      src: "@plugins/iview.js"
     },
     {
-      src: '@plugins/waterfall.js',
+      src: "@plugins/waterfall.js",
       ssr: false
     },
     {
-      src: '@plugins/v-charts.js',
+      src: "@plugins/v-charts.js",
       ssr: false
     },
-    { src: '~/plugins/swiper.js', ssr: false },
-    { src: '~/plugins/vuedraggable.js', ssr: false },
-    { src: '~/plugins/vue-swal.js' },
+    { src: "~/plugins/swiper.js", ssr: false },
+    { src: "~/plugins/vuedraggable.js", ssr: false },
+    { src: "~/plugins/vue-swal.js" },
     {
-      src: '~plugins/ant-design-vue.js'
+      src: "~plugins/ant-design-vue.js"
     }
   ],
 
@@ -94,16 +94,16 @@ module.exports = {
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     },
     /*
 ** Add vue-swal
 */
-    vendor: ['vue-swal']
+    vendor: ["vue-swal"]
   }
-}
+};
